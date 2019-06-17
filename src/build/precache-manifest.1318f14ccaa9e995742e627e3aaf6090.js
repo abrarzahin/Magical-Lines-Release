2aca79554d7c21533e88dbd6fcef0a15a2b6cceb
abrar zahin <abrarzahin@yahoo.com>
2019-06-16 20:07:58 -0400

    production ready

---
+++ src/build/precache-manifest.1318f14ccaa9e995742e627e3aaf6090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d168fe1d588de6e76eed7eeb74a1836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efca8725a4771cd28c93",
+    "url": "/static/css/main.7a41aa84.chunk.css"
+  },
+  {
+    "revision": "29f2dd377d2ab554ba3e",
+    "url": "/static/js/2.01c54b2c.chunk.js"
+  },
+  {
+    "revision": "efca8725a4771cd28c93",
+    "url": "/static/js/main.c33cc914.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
